94d4422eedfede2f3d06dced1331194f5a3c6632
joshua noble <joshuajnoble@gmail.com>
2012-01-15 17:22:30 -0800

    switch statement snuck in there from the previous 1.5 version

--- 01_ShapeApp/resources/geom.glsl
+++ 01_ShapeApp/resources/geom.glsl
@@ -27,83 +27,78 @@ void main(void)
 		// you'll probably not want to apply aspect all over.
 
 		// Key on shape
-		switch (shape)
+		if (shape == SHAPE_TRIANGLE)
 		{
 
-			case SHAPE_TRIANGLE:
 
-				// To draw a triangle, we add the size times the cos 
-				// and sin of each angle to the original position.
-				// After each point is set, we call EmitVertex() to
-				// add a vertex to the geometry. Be sure to draw 
-				// counter-clockwise.
-				gl_Position = gl_PositionIn[0] + vec4(cos(radians(270.0)) * size, sin(radians(270.0)) * size * aspect, 0.0, 0.0);
+			// To draw a triangle, we add the size times the cos 
+			// and sin of each angle to the original position.
+			// After each point is set, we call EmitVertex() to
+			// add a vertex to the geometry. Be sure to draw 
+			// counter-clockwise.
+			gl_Position = gl_PositionIn[0] + vec4(cos(radians(270.0)) * size, sin(radians(270.0)) * size * aspect, 0.0, 0.0);
+			EmitVertex();
+			gl_Position = gl_PositionIn[0] + vec4(cos(radians(150.0)) * size, sin(radians(150.0)) * size * aspect, 0.0, 0.0);
+			EmitVertex();
+			gl_Position = gl_PositionIn[0] + vec4(cos(radians(30.0)) * size, sin(radians(30.0)) * size * aspect, 0.0, 0.0);
+			EmitVertex();
+			
+			// Call EndPrimitive() to close the shape. Note that this 
+			// is not needed if you are only drawing one primitive, since 
+			// it is implied
+			EndPrimitive();
+
+		} else if (shape == SHAPE_SQUARE) {
+
+			// To draw a square, we will draw two triangles, just like 
+			// above. The two triangles will share vertices, so we'll define
+			// the vertices in advance.
+			vec4 vert0 = vec4(cos(radians(315.0)) * size, sin(radians(315.0)) * size * aspect, 0.0, 0.0);
+			vec4 vert1 = vec4(cos(radians(225.0)) * size, sin(radians(225.0)) * size * aspect, 0.0, 0.0);
+			vec4 vert2 = vec4(cos(radians(135.0)) * size, sin(radians(135.0)) * size * aspect, 0.0, 0.0);
+			vec4 vert3 = vec4(cos(radians(45.0)) * size, sin(radians(45.0)) * size * aspect, 0.0, 0.0);
+
+			// Draw the left triangle
+			gl_Position = gl_PositionIn[0] + vert0;
+			EmitVertex();
+			gl_Position = gl_PositionIn[0] + vert1;
+			EmitVertex();
+			gl_Position = gl_PositionIn[0] + vert3;
+			EmitVertex();
+
+			// Close this triangle
+			EndPrimitive();
+			
+			// And now the right one
+			gl_Position = gl_PositionIn[0] + vert3;
+			EmitVertex();
+			gl_Position = gl_PositionIn[0] + vert1;
+			EmitVertex();
+			gl_Position = gl_PositionIn[0] + vert2;
+			EmitVertex();
+
+			// Close the second triangle to form a quad
+			EndPrimitive();
+
+		} else if( shape == SHAPE_CIRCLE) {
+
+			// To draw a circle, we'll iterate through 24
+			// segments and form triangles
+			float twoPi = 6.283185306;
+			float delta = twoPi / 24.0;
+			for (float theta = 0.0; theta < twoPi; theta += delta)
+			{
+
+				// Draw a triangle to form a wedge of the circle
+				gl_Position = gl_PositionIn[0] + vec4(cos(theta) * size, sin(theta) * size * aspect, 0.0, 0.0);
 				EmitVertex();
-				gl_Position = gl_PositionIn[0] + vec4(cos(radians(150.0)) * size, sin(radians(150.0)) * size * aspect, 0.0, 0.0);
+				gl_Position = gl_PositionIn[0];
 				EmitVertex();
-				gl_Position = gl_PositionIn[0] + vec4(cos(radians(30.0)) * size, sin(radians(30.0)) * size * aspect, 0.0, 0.0);
+				gl_Position = gl_PositionIn[0] + vec4(cos(theta + delta) * size, sin(theta + delta) * size * aspect, 0.0, 0.0);
 				EmitVertex();
-				
-				// Call EndPrimitive() to close the shape. Note that this 
-				// is not needed if you are only drawing one primitive, since 
-				// it is implied
 				EndPrimitive();
 
-			break;
-			case SHAPE_SQUARE:
-
-				// To draw a square, we will draw two triangles, just like 
-				// above. The two triangles will share vertices, so we'll define
-				// the vertices in advance.
-				vec4 vert0 = vec4(cos(radians(315.0)) * size, sin(radians(315.0)) * size * aspect, 0.0, 0.0);
-				vec4 vert1 = vec4(cos(radians(225.0)) * size, sin(radians(225.0)) * size * aspect, 0.0, 0.0);
-				vec4 vert2 = vec4(cos(radians(135.0)) * size, sin(radians(135.0)) * size * aspect, 0.0, 0.0);
-				vec4 vert3 = vec4(cos(radians(45.0)) * size, sin(radians(45.0)) * size * aspect, 0.0, 0.0);
-
-				// Draw the left triangle
-				gl_Position = gl_PositionIn[0] + vert0;
-				EmitVertex();
-				gl_Position = gl_PositionIn[0] + vert1;
-				EmitVertex();
-				gl_Position = gl_PositionIn[0] + vert3;
-				EmitVertex();
-
-				// Close this triangle
-				EndPrimitive();
-				
-				// And now the right one
-				gl_Position = gl_PositionIn[0] + vert3;
-				EmitVertex();
-				gl_Position = gl_PositionIn[0] + vert1;
-				EmitVertex();
-				gl_Position = gl_PositionIn[0] + vert2;
-				EmitVertex();
-
-				// Close the second triangle to form a quad
-				EndPrimitive();
-
-			break;
-			case SHAPE_CIRCLE:
-
-				// To draw a circle, we'll iterate through 24
-				// segments and form triangles
-				float twoPi = 6.283185306;
-				float delta = twoPi / 24.0;
-				for (float theta = 0.0; theta < twoPi; theta += delta)
-				{
-
-					// Draw a triangle to form a wedge of the circle
-					gl_Position = gl_PositionIn[0] + vec4(cos(theta) * size, sin(theta) * size * aspect, 0.0, 0.0);
-					EmitVertex();
-					gl_Position = gl_PositionIn[0];
-					EmitVertex();
-					gl_Position = gl_PositionIn[0] + vec4(cos(theta + delta) * size, sin(theta + delta) * size * aspect, 0.0, 0.0);
-					EmitVertex();
-					EndPrimitive();
-
-				}
-
-			break;
+			}
 			
 		}
 
